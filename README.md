596d34e73bce0ac9b2ae5c029975f5ec5ee2c554
shobahus1 <shobahussolichin33@gmail.com>
2020-11-03 01:01:34 -0800

    hasil

--- README.md
+++ README.md
@@ -302,7 +302,9 @@ a=int(a)
 b=int(b)
 print("hasil pejumlahan {1}+{0}=%d".format(a,b) %(a+b))
 print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
+
 ```
+![hasil](Gambar1/hasil.PNG)
 <br>
 
 ### ============= THANK YOU ============= <br> 
--- shoba.py
+++ shoba.py
@@ -15,7 +15,7 @@ print("Nilai panjang = ", panjang)
 # penamaan
 
 nilai_y = 18 # dengan menggunakan underscore
-juta10 = 1000000 # ini boleh
+juta10 = 10000000 # ini boleh
 nilaiZ = 18.5 # ini boleh
 
 # pemanggilan kedua
@@ -28,6 +28,22 @@ print("Nilai b = ", a)
 
 b = a
 print("Nilai b = ",b)
+
+# assignment indirect
+b = a
+print("Nilai b = ",b)
+
+a=int(input("masukkan nilai a:"))
+b=int(input("masukkan nilai b:"))
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil pejumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
 
 
 
